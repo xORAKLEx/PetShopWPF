a3650940b6304150bf6ceba897f1de6edd5fb114
☞Коля Миколенко☜ <kolyan.mikolenko@mail.ru>
2022-04-27 22:21:29 +0300

    Init

---
+++ Interfacese/Models/IDog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfacese.Models
+{
+    public interface IDog
+    {
+        int id { get; }
+
+        int userId { get; }
+
+        string name { get; }
+
+
+    }
+}
