783ea3db7fa0837be927569343d5ed552634a0ae
Aleksandr Unhurian <unguryan.sasha@gmail.com>
2022-04-28 02:40:03 +0300

    Added Serializer

--- Services/Dog_GrpcService/Startup.cs
+++ Services/Dog_GrpcService/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Dog_GrpcService.Services;
+﻿using Core.Serializers;
+using Dog_GrpcService.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -18,6 +19,8 @@ namespace Dog_GrpcService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc();
+
+            services.AddTransient<DogSerializer>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
