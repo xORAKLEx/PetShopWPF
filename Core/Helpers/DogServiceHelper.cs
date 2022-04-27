586309e81a6aba0b15b328ea31f4119aa0b7c208
Aleksandr Unhurian <unguryan.sasha@gmail.com>
2022-04-28 02:36:37 +0300

    Updated DogGRPC Service, added helper

---
+++ Core/Helpers/DogServiceHelper.cs
@@ -0,0 +1,29 @@
+﻿using Core.Models;
+using interfaces.Protos;
+using Interfaces.Models;
+
+namespace Core.Helpers
+{
+    public static class DogServiceHelper
+    {
+        public static DogModel ToGrpc(this IDog dog)
+        {
+            return new DogModel()
+            {
+                Id = dog.Id,
+                Name = dog.Name,
+                UserId = dog.UserId
+            };
+        }
+
+        public static IDog FromGrpc(this DogModel dog)
+        {
+            return new Dog()
+            {
+                Id = dog.Id,
+                Name = dog.Name,
+                UserId = dog.UserId
+            };
+        }
+    }
+}
--- Services/Dog_GrpcService/Services/DogGRPC_Service.cs
+++ Services/Dog_GrpcService/Services/DogGRPC_Service.cs
@@ -1,6 +1,11 @@
-﻿using Grpc.Core;
+﻿using Core.Helpers;
+using Core.Serializers;
+using Grpc.Core;
 using interfaces.Protos;
+using Interfaces.Models;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Dog_GrpcService.Services
@@ -9,39 +14,112 @@ namespace Dog_GrpcService.Services
     {
         private readonly ILogger<DogGRPC_Service> _logger;
 
-        public DogGRPC_Service(ILogger<DogGRPC_Service> logger)
+        private readonly DogSerializer _serializer;
+
+        private IList<IDog> _dogs;
+
+        public DogGRPC_Service(ILogger<DogGRPC_Service> logger, DogSerializer serializer)
         {
             _logger = logger;
+            _serializer = serializer;
+            _dogs = _serializer.Deserialize();
         }
 
-        public override Task GetDogs(NullRequest request, IServerStreamWriter<DogModel> responseStream, ServerCallContext context)
+        public override async Task GetDogs(NullRequest request, IServerStreamWriter<DogModel> responseStream, ServerCallContext context)
         {
-            return base.GetDogs(request, responseStream, context);
+            foreach (var response in _dogs)
+            {
+                var model = new DogModel()
+                {
+                    Id= response.Id,
+                    Name = response.Name,
+                    UserId = response.UserId
+                };
+
+                await responseStream.WriteAsync(model);
+            }
         }
 
-        public override Task GetUserDogs(GetUserDogRequest request, IServerStreamWriter<DogModel> responseStream, ServerCallContext context)
+        public override async Task GetUserDogs(GetUserDogRequest request, IServerStreamWriter<DogModel> responseStream, ServerCallContext context)
         {
-            return base.GetUserDogs(request, responseStream, context);
+            if(!_dogs.Any(d => d.UserId == request.UserId))
+            {
+                throw new RpcException(Status.DefaultCancelled, "User do not have dogs");
+            }
+
+            foreach (var response in _dogs.Where(d => d.UserId == request.UserId))
+            {
+                var model = new DogModel()
+                {
+                    Id = response.Id,
+                    Name = response.Name,
+                    UserId = response.UserId
+                };
+
+                await responseStream.WriteAsync(model);
+            }
         }
 
-        public override Task<DogModel> GetDog(DogIdRequest request, ServerCallContext context)
+        public override async Task<DogModel> GetDog(DogIdRequest request, ServerCallContext context)
         {
-            return base.GetDog(request, context);
+            if (!_dogs.Any(d => d.Id == request.Id))
+            {
+                throw new RpcException(Status.DefaultCancelled, "Dog do not found");
+            }
+            return _dogs.First(d => d.Id == request.Id).ToGrpc();
         }
 
-        public override Task<DogModel> AddDog(AddDogRequest request, ServerCallContext context)
+        public override async Task<DogModel> AddDog(AddDogRequest request, ServerCallContext context)
         {
-            return base.AddDog(request, context);
+            int id = _dogs.Any() ? _dogs.Max(d => d.Id) + 1 : 0;
+            var dog = new DogModel()
+            {
+                Id = id,
+                Name = request.Name,
+                UserId = request.UserId
+            };
+
+            _dogs.Add(dog.FromGrpc());
+
+            _serializer.Serialize(_dogs);
+
+            return dog;
         }
 
-        public override Task<DogModel> RemoveDog(DogIdRequest request, ServerCallContext context)
+        public override async Task<DogModel> RemoveDog(DogIdRequest request, ServerCallContext context)
         {
-            return base.RemoveDog(request, context);
+            if (!_dogs.Any(d => d.Id == request.Id))
+            {
+                throw new RpcException(Status.DefaultCancelled, "Dog cannot be removed. Not found");
+            }
+
+            var dog = _dogs.First(d => d.Id == request.Id);
+
+            _dogs.Remove(dog);
+
+            _serializer.Serialize(_dogs);
+
+            return dog.ToGrpc();
         }
 
-        public override Task<DogModel> AdoptDog(AdoptDogRequest request, ServerCallContext context)
+        public override async Task<DogModel> AdoptDog(AdoptDogRequest request, ServerCallContext context)
         {
-            return base.AdoptDog(request, context);
+            if (!_dogs.Any(d => d.Id == request.Id))
+            {
+                throw new RpcException(Status.DefaultCancelled, "Dog cannot be adopted. Not found");
+            }
+
+            var dog = _dogs.First(d => d.Id == request.Id);
+
+            //Разница?
+            var index = _dogs.IndexOf(dog);
+            _dogs[index].UserId = request.UserId;
+            //или так можно??
+            dog.UserId = request.UserId;
+
+            _serializer.Serialize(_dogs);
+
+            return dog.ToGrpc();
         }
     }
 }
